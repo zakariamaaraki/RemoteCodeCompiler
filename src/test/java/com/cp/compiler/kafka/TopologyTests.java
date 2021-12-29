6b35d2e2c982588fc93e7061b5841062c574b7c3
Maaraki Zakaria <zmaaraki@gmail.com>
2021-12-29 13:58:36 +0100

    add kafka streams tests

--- src/test/java/com/cp/compiler/kafka/TopologyTests.java
+++ src/test/java/com/cp/compiler/kafka/TopologyTests.java
@@ -1,23 +1,33 @@
 package com.cp.compiler.kafka;
 
+import com.cp.compiler.controllers.CompilerController;
+import com.cp.compiler.exceptions.CompilerServerException;
 import com.cp.compiler.models.Language;
 import com.cp.compiler.models.Request;
+import com.cp.compiler.models.Response;
+import com.cp.compiler.services.CompilerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.TestInputTopic;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 @ActiveProfiles("kafka")
@@ -27,22 +37,23 @@ public class TopologyTests {
 	@Autowired
 	private Topology topology;
 	
+	@InjectMocks
+	private CompilerController compilerController;
+	
+	@Mock
+	private CompilerService compilerService;
+	
+	@Mock
+	private MultipartFile outputFile;
+	
+	@Mock
+	private MultipartFile sourceCode;
+	
 	private TopologyTestDriver streamTest;
 	private TestInputTopic<String, String> inputTopic;
+	private TestOutputTopic<String, String> outputTopic;
 	
 	private final Serde<String> stringSerde = new Serdes.StringSerde();
-	
-	// Expected output
-	private File outputResource = new ClassPathResource("outputs/Test1.txt").getFile();
-	private String expectedOutput = new String(Files.readAllBytes(outputResource.toPath()));
-	
-	// Java source code
-	private File sourceCodeResource = new ClassPathResource("sources/java/Test1.java").getFile();
-	private String sourceCode = new String(Files.readAllBytes(sourceCodeResource.toPath()));
-	
-	private ObjectMapper objectMapper = new ObjectMapper();
-	
-	private Request request = new Request(null, expectedOutput, sourceCode, Language.JAVA, 10, 500);
 	
 	public TopologyTests() throws IOException {}
 	
@@ -59,5 +70,29 @@ public class TopologyTests {
 		
 		// setup test topics
 		inputTopic = streamTest.createInputTopic("kafka.topic.input", stringSerde.serializer(), stringSerde.serializer());
+		outputTopic = streamTest.createOutputTopic("kafka.topic.output", stringSerde.deserializer(), stringSerde.deserializer());
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		streamTest.close();
+	}
+	
+	@Test
+	public void shouldConsumeMessageFromInputTopicAndProduceMessageToOutputTopic() throws CompilerServerException {
+
+		// Given
+		Mockito.when(compilerService.compile(outputFile, sourceCode, null, 10, 500, Language.JAVA))
+				.thenReturn(ResponseEntity
+						.status(HttpStatus.OK)
+						.body(new Response("test output", "test expected output", "Accepted", LocalDateTime.now())));
+		String jsonRequest = "{\n\"expectedOutput\": \"0\\n1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n\",\n\"sourceCode\": \"public class Test1 {\\npublic static void main(String[] args) {\\nint i = 0;\\nwhile (i < 10) {\\nSystem.out.println(i++);\\n}}}\",\n\"language\": \"JAVA\",\"timeLimit\": 15,\"memoryLimit\": 500\n}";
+		
+		// When
+		inputTopic.pipeInput(jsonRequest);
+		
+		// Then
+		Assertions.assertThat(!outputTopic.isEmpty());
+		
 	}
 }
