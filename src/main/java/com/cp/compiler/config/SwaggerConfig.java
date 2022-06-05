afe01ee06775c77a318af51a0efe388c53ac22c3
Maaraki Zakaria <zmaaraki@gmail.com>
2022-06-05 19:29:40 +0200

    update swagger config

--- src/main/java/com/cp/compiler/config/SwaggerConfig.java
+++ src/main/java/com/cp/compiler/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.cp.compiler.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +19,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class SwaggerConfig {
     
+    private static final String TITLE = "Remote Code Compiler";
+    
+    private static final String DESCRIPTION = "An online code compiler supporting Java, C, C++, Golang and Python " +
+            "for competitive programming and coding interviews.";
+    
+    private static final String VERSION = "1.0.0";
+    
+    private static final String LICENCE = "GPL-3.0 Licence";
+    
+    private static final String LICENCE_URL = "https://github.com/zakariamaaraki/RemoteCodeCompiler/blob/master/LICENSE";
+    
     /**
      * Api docket.
      *
@@ -28,6 +41,17 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cp.compiler.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+    
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version(VERSION)
+                .license(LICENCE)
+                .licenseUrl(LICENCE_URL)
                 .build();
     }
 }
