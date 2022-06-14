0e1b4857c9e3b600e6c8a64d52bec3396f7d0690
Maaraki Zakaria <zmaaraki@gmail.com>
2022-06-14 20:35:06 +0200

    add javadoc to WellKnownMetrics class

--- src/main/java/com/cp/compiler/models/WellKnownMetrics.java
+++ src/main/java/com/cp/compiler/models/WellKnownMetrics.java
@@ -40,19 +40,43 @@ public abstract class WellKnownMetrics {
      */
     public static final String CS_COUNTER_NAME = "cs.counter";
     
+    /**
+     * The constant THROTTLING_COUNTER_NAME.
+     */
     public static final String THROTTLING_COUNTER_NAME = "throttling.counter";
     
+    /**
+     * The constant EXECUTIONS_GAUGE.
+     */
     public static final String EXECUTIONS_GAUGE = "executions";
     
+    /**
+     * The constant SHORT_RUNNING_EXECUTIONS_COUNTER.
+     */
     public static final String SHORT_RUNNING_EXECUTIONS_COUNTER = "short-running-executions.counter";
     
+    /**
+     * The constant LONG_RUNNING_EXECUTIONS_COUNTER.
+     */
     public static final String LONG_RUNNING_EXECUTIONS_COUNTER = "long-running-executions.counter";
     
+    /**
+     * The constant CONTAINER_BUILD_TIMER.
+     */
     public static final String CONTAINER_BUILD_TIMER = "container.build";
     
+    /**
+     * The constant CONTAINER_RUN_TIMER.
+     */
     public static final String CONTAINER_RUN_TIMER = "container.build";
     
+    /**
+     * The constant KAFKA_THROTTLING_RETRIES.
+     */
     public static final String KAFKA_THROTTLING_RETRIES = "kafka.throttling.retries";
     
+    /**
+     * The constant AMQP_THROTTLING_RETRIES.
+     */
     public static final String AMQP_THROTTLING_RETRIES = "amqp.throttling.retries";
 }
