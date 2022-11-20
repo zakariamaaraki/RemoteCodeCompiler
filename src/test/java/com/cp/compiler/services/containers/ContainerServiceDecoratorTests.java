b4a6543f2fcc6d66bb7e97541ff9678304f51718
Maaraki Zakaria <zmaaraki@gmail.com>
2022-11-20 18:35:36 +0100

    [ContainerServiceDecorator] Add UT for common methods

---
+++ src/test/java/com/cp/compiler/services/containers/ContainerServiceDecoratorTests.java
@@ -0,0 +1,136 @@
+package com.cp.compiler.services.containers;
+
+import com.cp.compiler.models.ProcessOutput;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ContainerServiceDecoratorTests {
+    
+    @Test
+    void getRunningContainersShouldReturnSameResult() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        String expectedOutput = "runningContainers";
+        Mockito.when(containerServiceMock.getRunningContainers()).thenReturn(expectedOutput);
+        
+        // When
+        String output = containerServiceDecorator.getRunningContainers();
+        
+        // Then
+        Assertions.assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void getImagesShouldHaveTheSameBehaviour() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        String expectedOutput = "images";
+        Mockito.when(containerServiceMock.getImages()).thenReturn(expectedOutput);
+        
+        // When
+        String output = containerServiceDecorator.getImages();
+        
+        // Then
+        Assertions.assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void getContainersStatsShouldHaveTheSameBehaviour() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        String expectedOutput = "containers stats";
+        Mockito.when(containerServiceMock.getContainersStats()).thenReturn(expectedOutput);
+        
+        // When
+        String output = containerServiceDecorator.getContainersStats();
+        
+        // Then
+        Assertions.assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void getALlContainersStatsShouldHaveTheSameBehaviour() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        String expectedOutput = "all containers stats";
+        Mockito.when(containerServiceMock.getAllContainersStats()).thenReturn(expectedOutput);
+        
+        // When
+        String output = containerServiceDecorator.getAllContainersStats();
+        
+        // Then
+        Assertions.assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void deleteImageShouldHaveTheSameBehaviour() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        String expectedOutput = "image deleted";
+        Mockito.when(containerServiceMock.deleteImage("image")).thenReturn(expectedOutput);
+        
+        // When
+        String output = containerServiceDecorator.deleteImage("image");
+        
+        // Then
+        Assertions.assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void isUpShouldHaveTheSameBehaviour() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        Mockito.when(containerServiceMock.isUp()).thenReturn(true);
+        
+        // When
+        boolean output = containerServiceDecorator.isUp();
+        
+        // Then
+        Assertions.assertEquals(true, output);
+    }
+    
+    @Test
+    void getContainerizationNameShouldHaveTheSameBehaviour() {
+        // Given
+        var containerServiceMock = Mockito.mock(ContainerService.class);
+        ContainerServiceDecorator containerServiceDecorator = getContainerServiceDecorator(containerServiceMock);
+        String expectedOutput = "docker";
+        Mockito.when(containerServiceMock.getContainerizationName()).thenReturn(expectedOutput);
+        
+        // When
+        String output = containerServiceDecorator.getContainerizationName();
+        
+        // Then
+        Assertions.assertEquals(expectedOutput, output);
+    }
+    
+    private ContainerServiceDecorator getContainerServiceDecorator(ContainerService containerServiceMock) {
+        return new ContainerServiceDecorator(containerServiceMock) {
+            @Override
+            public String buildImage(String contextPath, String imageName, String dockerfileName) {
+                return null;
+            }
+            
+            @Override
+            public ProcessOutput runContainer(String imageName, long timeout, float maxCpus) {
+                return null;
+            }
+            
+            @Override
+            public ProcessOutput runContainer(String imageName,
+                                              long timeout,
+                                              String volumeMounting,
+                                              String executionPath,
+                                              String sourceCodeFileName) {
+                return null;
+            }
+        };
+    }
+}
