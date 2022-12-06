7ebd0443507cac6b124f7a0162711178db8fb2ff
Maaraki Zakaria <zmaaraki@gmail.com>
2022-12-06 23:21:38 +0100

    Add UT tests for models

---
+++ src/test/java/com/cp/compiler/models/ResponseTests.java
@@ -0,0 +1,74 @@
+package com.cp.compiler.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+
+public class ResponseTests {
+    
+    @Test
+    void shouldComputeTheAverageExecutionDuration() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+    
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                150);
+        
+        // When
+        var response = new Response(
+                Verdict.ACCEPTED.getStatusResponse(),
+                Verdict.ACCEPTED.getStatusCode(),
+                "",
+                new LinkedHashMap<>() {{
+                    put("test1", testCaseResult1);
+                    put("test2", testCaseResult2);
+                }},
+                100,
+                15,
+                500,
+                Language.JAVA,
+                LocalDateTime.now());
+        
+        // Then
+        Assertions.assertEquals(125, response.getAverageExecutionDuration());
+    }
+    
+    @Test
+    void shouldConvertTimeLimitToMillis() {
+        // Given
+        var testCaseResult = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        // When
+        var response = new Response(
+                Verdict.ACCEPTED.getStatusResponse(),
+                Verdict.ACCEPTED.getStatusCode(),
+                "",
+                new LinkedHashMap<>() {{
+                    put("test1", testCaseResult);
+                }},
+                100,
+                15,
+                500,
+                Language.JAVA,
+                LocalDateTime.now());
+        
+        // Then
+        Assertions.assertEquals(15 * 1000, response.getTimeLimit());
+    }
+}
---
+++ src/test/java/com/cp/compiler/models/TestCaseResultTests.java
@@ -0,0 +1,151 @@
+package com.cp.compiler.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCaseResultTests {
+
+    @Test
+    void shouldReturnTrue() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+    
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        // When
+        boolean equals = testCaseResult1.equals(testCaseResult2);
+        
+        // Then
+        Assertions.assertTrue(equals);
+    }
+    
+    @Test
+    void ifVerdictIsDifferentShouldReturnFalse() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.WRONG_ANSWER,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        // When
+        boolean equals = testCaseResult1.equals(testCaseResult2);
+        
+        // Then
+        Assertions.assertFalse(equals);
+    }
+    
+    @Test
+    void ifOutputIsDifferentShouldReturnFalse() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output2",
+                "error",
+                "expectedOutput",
+                100);
+        
+        // When
+        boolean equals = testCaseResult1.equals(testCaseResult2);
+        
+        // Then
+        Assertions.assertFalse(equals);
+    }
+    
+    @Test
+    void ifErrorIsDifferentShouldReturnFalse() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error2",
+                "expectedOutput",
+                100);
+        
+        // When
+        boolean equals = testCaseResult1.equals(testCaseResult2);
+        
+        // Then
+        Assertions.assertFalse(equals);
+    }
+    
+    @Test
+    void ifExpectedOutputIsDifferentShouldReturnFalse() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput2",
+                100);
+        
+        // When
+        boolean equals = testCaseResult1.equals(testCaseResult2);
+        
+        // Then
+        Assertions.assertFalse(equals);
+    }
+    
+    @Test
+    void ifExecutionDurationIsDifferentShouldReturnFalse() {
+        // Given
+        var testCaseResult1 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                100);
+        
+        var testCaseResult2 = new TestCaseResult(
+                Verdict.ACCEPTED,
+                "output",
+                "error",
+                "expectedOutput",
+                90);
+        
+        // When
+        boolean equals = testCaseResult1.equals(testCaseResult2);
+        
+        // Then
+        Assertions.assertFalse(equals);
+    }
+}
