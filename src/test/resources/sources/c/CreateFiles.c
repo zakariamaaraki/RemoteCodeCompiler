de87d5374a2633b11720b640100dd3599e18e0c5
Maaraki Zakaria <zmaaraki@gmail.com>
2023-01-22 01:43:15 +0100

    do not use root user in dockerfiles

---
+++ src/test/resources/sources/c/CreateFiles.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() {
+   FILE *fp;
+
+   fp = fopen("/tmp/test.txt", "w+");
+   fprintf(fp, "This is testing for fprintf...\n");
+   fputs("This is testing for fputs...\n", fp);
+   fclose(fp);
+}
